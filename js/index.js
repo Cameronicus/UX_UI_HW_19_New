94fa643ba93f867081bb625e246c2998524cf450
Cameron Slaven <cameron@playsidestudios.com>
2021-10-11 14:39:45 +1100

    Addition

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function closeN() {
+document.getElementById("sideNav").style.width = "0";
+document.getElementById("main").style.marginLeft = "0";
+}
+
+function openN() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
