e489e418408e63378352138a2649fb3fb5eb5e21
Cameron Slaven <cameron@playsidestudios.com>
2021-10-14 22:13:31 +1100

    halp

--- js/index.js
+++ js/index.js
@@ -8,25 +8,28 @@ function openN() {
     document.getElementById("main").style.marginLeft = "250px";
   }
 
-  // Set object for the modal
-var myModal = document.getElementById("contModal");
-// Set object for the button that will trigger the modal box
-var myButton = document.getElementById("buttonMod");
-// Set an element that will close the modal box
-var exitBtn = document.getElementsByClassName("close")[0];
-// Allows display of modal box, when user clicks the button
-myButton.onclick = function() {
-myModal.style.display = "block";
-}
-// Allows the user to close the modal box, when user will click on (x) button
-exitBtn.onclick = function() {
-myModal.style.display = "none";
-}
-// Allows the user to close the modal box, even when the user clicks anywhere outside of the modal box
-window.onclick = function(event) {
-if (event.target == myModal) {
-myModal.style.display = "none";
-}
+ // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-  
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
