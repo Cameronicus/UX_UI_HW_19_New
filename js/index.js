7cf1ec29af68df070efe413e551f3e19e4fe3616
Cameron Slaven <cameron@playsidestudios.com>
2021-10-14 21:21:40 +1100

    moving stuff around

--- js/index.js
+++ js/index.js
@@ -16,4 +16,16 @@ function openN() {
     });
   });
 
-  
+  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-SYEBNMR1ED');
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:2651388,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
