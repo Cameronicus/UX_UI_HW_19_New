807e18da2f7bfacb530f159cad9569574c3fb303
Cameron Slaven <cameron@playsidestudios.com>
2021-10-14 21:52:34 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,12 +8,25 @@ function openN() {
     document.getElementById("main").style.marginLeft = "250px";
   }
 
-  $("#contModal").click(function(event) {
-    event.preventDefault();
-    this.blur();
-    $.get(this.button, function(html) {
-       $(button).appendTo("body").modal();
-    });
-  });
+  // Set object for the modal
+var myModal = document.getElementById("contModal");
+// Set object for the button that will trigger the modal box
+var myButton = document.getElementById("buttonMod");
+// Set an element that will close the modal box
+var exitBtn = document.getElementsByClassName("buttonClose")[0];
+// Allows display of modal box, when user clicks the button
+myButton.onclick = function() {
+myModal.style.display = "block";
+}
+// Allows the user to close the modal box, when user will click on (x) button
+exitBtn.onclick = function() {
+myModal.style.display = "none";
+}
+// Allows the user to close the modal box, even when the user clicks anywhere outside of the modal box
+window.onclick = function(event) {
+if (event.target == myModal) {
+myModal.style.display = "none";
+}
+}
 
   
